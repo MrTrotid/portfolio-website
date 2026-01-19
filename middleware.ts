2204d4c8428964ee01bf8d1e6c59bf5f64752b75
MrTrotid <bamanguragain@gmail.com>
2026-01-19 14:13:14 +0545

    fixes

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
 
   // If subdomain is resume.*, redirect to resume PDF
   if (hostname?.startsWith('resume.')) {
-    return NextResponse.redirect(new URL('/resume.pdf', request.url), { status: 301 });
+    return NextResponse.redirect('https://bamanguragain.com.np/resume.pdf', { status: 301 });
   }
 
   return NextResponse.next();
